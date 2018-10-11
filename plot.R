2277a6dc6c71e7ba5099707e9d39ce851caac188
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:11:49 -0400

    Saving plot.R

--- plot.R
+++ plot.R
@@ -3,3 +3,6 @@ library(ggplot2)
 #plotting histgram of population with specifying color,fill and binwidth
 plot1<-ggplot(final_df,aes(x=population)) +
   geom_histogram(color="black",fill="red",bin=20)
+
+plot2<-ggplot(df_final,aes(x=Murder)) +
+  geom_histogram(color="black",fill="red",bin=20)
